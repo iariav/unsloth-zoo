90f9318ee0555a1d277f2b613f0e9a12a6b61ae3
iariav <idoariav@gmail.com>
2025-05-04 09:36:47 +0300

    update vision utils

--- unsloth_zoo/vision_utils.py
+++ unsloth_zoo/vision_utils.py
@@ -35,32 +35,36 @@ __all__ = [
 
 global IMAGE_TOKENS
 IMAGE_TOKENS = [
-    "<|image|>",          # Llama 3.2 Vision, Phi 3.5
-    "<|vision_start|>",   # Qwen
-    "<|vision_end|>",     # Qwen
-    "<|vision_pad|>",     # Qwen
-    "<|image_pad|>",      # Qwen
-    "<|video_pad|>",      # Qwen
-    "<image>",            # PaliGemma / Llava
-    "[IMG]",              # Mistral
-    "[IMG_BREAK]",        # Mistral
-    "[IMG_END]",          # Mistral
-    "<image_soft_token>", # Gemma 3
-    "<start_of_image>",   # Gemma 3
-    "<end_of_image>",     # Gemma 3
-    "<|START_OF_IMG|>",   # Cohere
-    "<|END_OF_IMG|>",     # Cohere
-    "<|IMG_LINE_BREAK|>", # Cohere
-    "<|IMG_PATCH|>",      # Cohere
+    "<|image|>",  # Llama 3.2 Vision, Phi 3.5
+    "<|vision_start|>",  # Qwen
+    "<|vision_end|>",  # Qwen
+    "<|vision_pad|>",  # Qwen
+    "<|image_pad|>",  # Qwen
+    "<|video_pad|>",  # Qwen
+    "<image>",  # PaliGemma / Llava
+    "[IMG]",  # Mistral
+    "[IMG_BREAK]",  # Mistral
+    "[IMG_END]",  # Mistral
+    "<image_soft_token>",  # Gemma 3
+    "<start_of_image>",  # Gemma 3
+    "<end_of_image>",  # Gemma 3
+    "<|START_OF_IMG|>",  # Cohere
+    "<|END_OF_IMG|>",  # Cohere
+    "<|IMG_LINE_BREAK|>",  # Cohere
+    "<|IMG_PATCH|>",  # Cohere
 ]
 
 import torch
 from PIL import Image
 import base64
 from io import BytesIO
+import os
 import math
 import requests
 from typing import Union, Tuple
+from qwen_vl_utils.vision_process import fetch_video
+from transformers import Qwen2VLProcessor
+
 IMAGE_FACTOR = 28
 MIN_PIXELS = 4 * 28 * 28
 MAX_PIXELS = 16384 * 28 * 28
@@ -78,21 +82,29 @@ FPS_MAX_FRAMES = 768
 def round_by_factor(number: int, factor: int) -> int:
     """Returns the closest integer to 'number' that is divisible by 'factor'."""
     return round(number / factor) * factor
+
+
 pass
+
 
 def ceil_by_factor(number: int, factor: int) -> int:
     """Returns the smallest integer greater than or equal to 'number' that is divisible by 'factor'."""
     return math.ceil(number / factor) * factor
+
+
 pass
+
 
 def floor_by_factor(number: int, factor: int) -> int:
     """Returns the largest integer less than or equal to 'number' that is divisible by 'factor'."""
     return math.floor(number / factor) * factor
+
+
 pass
 
 
 def smart_resize(
-    height: int, width: int, factor: int = IMAGE_FACTOR, min_pixels: int = MIN_PIXELS, max_pixels: int = MAX_PIXELS
+        height: int, width: int, factor: int = IMAGE_FACTOR, min_pixels: int = MIN_PIXELS, max_pixels: int = MAX_PIXELS
 ) -> tuple[int, int]:
     """
     Rescales the image so that the following conditions are met:
@@ -118,12 +130,14 @@ def smart_resize(
         h_bar = ceil_by_factor(height * beta, factor)
         w_bar = ceil_by_factor(width * beta, factor)
     return h_bar, w_bar
+
+
 pass
 
 
 def fetch_image(
-    ele: dict[Union[Tuple[str, str], Image.Image]],
-    size_factor: int = IMAGE_FACTOR,
+        ele: dict[Union[Tuple[str, str], Image.Image]],
+        size_factor: int = IMAGE_FACTOR,
 ) -> Image.Image:
     if "image" in ele:
         image = ele["image"]
@@ -170,6 +184,8 @@ def fetch_image(
     image = image.resize((resized_width, resized_height))
 
     return image
+
+
 pass
 
 
@@ -182,36 +198,40 @@ def extract_vision_info(conversations: Union[list[dict], list[list[dict]]]) -> l
             if isinstance(message["content"], list):
                 for ele in message["content"]:
                     if (
-                        "image" in ele
-                        or "image_url" in ele
-                        or "video" in ele
-                        or ele["type"] in ("image", "image_url", "video")
+                            "image" in ele
+                            or "image_url" in ele
+                            or "video" in ele
+                            or ele["type"] in ("image", "image_url", "video")
                     ):
                         vision_infos.append(ele)
     return vision_infos
-pass
 
 
-def process_vision_info(
-    conversations: Union[list[dict], list[list[dict]]],
-) -> tuple[Union[list[Image.Image], None], Union[list[Union[torch.Tensor, list[Image.Image]]], None]]:
-    vision_infos = extract_vision_info(conversations)
-    ## Read images or videos
-    image_inputs = []
-    video_inputs = []
-    for vision_info in vision_infos:
-        if "image" in vision_info or "image_url" in vision_info:
-            image_inputs.append(fetch_image(vision_info))
-        elif "video" in vision_info:
-            video_inputs.append(fetch_video(vision_info))
-        else:
-            raise ValueError("image, image_url or video should in content.")
-    if len(image_inputs) == 0:
-        image_inputs = None
-    if len(video_inputs) == 0:
-        video_inputs = None
-    return image_inputs, video_inputs
 pass
+
+from qwen_vl_utils import process_vision_info
+
+
+# def process_vision_info(
+#     conversations: Union[list[dict], list[list[dict]]],
+# ) -> tuple[Union[list[Image.Image], None], Union[list[Union[torch.Tensor, list[Image.Image]]], None]]:
+#     vision_infos = extract_vision_info(conversations)
+#     ## Read images or videos
+#     image_inputs = []
+#     video_inputs = []
+#     for vision_info in vision_infos:
+#         if "image" in vision_info or "image_url" in vision_info:
+#             image_inputs.append(fetch_image(vision_info))
+#         elif "video" in vision_info:
+#             video_inputs.append(fetch_video(vision_info))
+#         else:
+#             raise ValueError("image, image_url or video should in content.")
+#     if len(image_inputs) == 0:
+#         image_inputs = None
+#     if len(video_inputs) == 0:
+#         video_inputs = None
+#     return image_inputs, video_inputs
+# pass
 
 
 def get_padding_tokens_ids(tokenizer):
@@ -232,6 +252,8 @@ def get_padding_tokens_ids(tokenizer):
     padding_token_ids = list(set(padding_token_ids))
     padding_token_ids = torch.IntTensor(padding_token_ids)
     return padding_token_ids
+
+
 pass
 
 
@@ -244,41 +266,47 @@ def _get_dtype(dtype):
         "bfloat16": torch.bfloat16,
         torch.bfloat16: torch.bfloat16,
     }
-    if   dtype is None or dtype == None: return None
-    elif dtype in __DTYPE_MAP: return __DTYPE_MAP[dtype]
+    if dtype is None or dtype == None:
+        return None
+    elif dtype in __DTYPE_MAP:
+        return __DTYPE_MAP[dtype]
     else:
         print(f"Unsloth: {dtype} is not recognized, so we'll default to None")
         return None
     pass
+
+
 pass
 
 import PIL.Image
+
 LANCZOS = PIL.Image.Resampling.LANCZOS
 from .dataset_utils import train_on_responses_only as _train_on_responses_only
+
 
 class UnslothVisionDataCollator:
     # All Unsloth Zoo code licensed under LGPLv3
     __slots__ = \
         "padding_token_ids", "dtype", "ignore_index", \
-        "processor", "formatting_func", "image_size", \
-        "max_seq_length", "truncation", "train_on_responses_only", \
-        "num_proc", "assistant_single_content",
+            "processor", "formatting_func", "image_size", \
+            "max_seq_length", "truncation", "train_on_responses_only", \
+            "num_proc", "assistant_single_content",
 
     def __init__(
-        self,
-        model,
-        processor,
-        max_seq_length  = None,
-        formatting_func = None,
-        resize = "min", # Can be (10, 10) or "min" to resize to fit
-                        # the model's default image_size or "max"
-                        # for no resizing and leave image intact
-        ignore_index = -100,
-        train_on_responses_only = False,
-        instruction_part = None,
-        response_part    = None,
-        force_match      = True, # Match newlines as well!
-        num_proc         = None,
+            self,
+            model,
+            processor,
+            max_seq_length=None,
+            formatting_func=None,
+            resize="min",  # Can be (10, 10) or "min" to resize to fit
+            # the model's default image_size or "max"
+            # for no resizing and leave image intact
+            ignore_index=-100,
+            train_on_responses_only=False,
+            instruction_part=None,
+            response_part=None,
+            force_match=True,  # Match newlines as well!
+            num_proc=None,
     ):
         if not hasattr(processor, "image_processor"):
             raise TypeError("Unsloth: UnslothVisionDataCollator is only for image models!")
@@ -286,8 +314,8 @@ class UnslothVisionDataCollator:
         self.padding_token_ids = get_padding_tokens_ids(processor)
         self.dtype = _get_dtype(
             model.config.torch_dtype \
-            if hasattr(model.config, "torch_dtype") else \
-            model.get_input_embeddings().weight.dtype
+                if hasattr(model.config, "torch_dtype") else \
+                model.get_input_embeddings().weight.dtype
         )
         self.ignore_index = ignore_index
         self.processor = processor
@@ -303,14 +331,14 @@ class UnslothVisionDataCollator:
         elif resize == "max":
             self.image_size = None
         elif type(resize) is tuple or type(resize) is list:
-            assert(len(resize) == 2)
-            assert(type(resize[0]) is int and type(resize[1]) is int)
+            assert (len(resize) == 2)
+            assert (type(resize[0]) is int and type(resize[1]) is int)
             self.image_size = tuple(resize)
         elif type(resize) is int:
             self.image_size = resize
         else:
             raise TypeError(
-                "Unsloth: resize accepts 'min', 'max', a tuple of 2 numbers or 1 number\n"\
+                "Unsloth: resize accepts 'min', 'max', a tuple of 2 numbers or 1 number\n" \
                 "For example (224, 224) or just 224. The default is 'min' which auto resizes images!"
             )
         pass
@@ -323,15 +351,15 @@ class UnslothVisionDataCollator:
 
         # Train on reponses if provided
         if train_on_responses_only:
-            assert(type(instruction_part) is str and type(response_part) is str)
+            assert (type(instruction_part) is str and type(response_part) is str)
             self.train_on_responses_only = _train_on_responses_only(
                 None,
-                instruction_part = instruction_part,
-                response_part    = response_part,
-                force_match      = force_match,
-                tokenizer        = processor,
-                return_function  = True,
-                num_proc         = num_proc,
+                instruction_part=instruction_part,
+                response_part=response_part,
+                force_match=force_match,
+                tokenizer=processor,
+                return_function=True,
+                num_proc=num_proc,
             )
         else:
             self.train_on_responses_only = None
@@ -357,8 +385,8 @@ class UnslothVisionDataCollator:
                 ])
                 self.assistant_single_content = True
                 print(
-                    f"Unsloth: {processor.__class__.__name__} only accepts 1 "\
-                    "text field for assistant roles!\n"\
+                    f"Unsloth: {processor.__class__.__name__} only accepts 1 " \
+                    "text field for assistant roles!\n" \
                     "We will auto fix the data collator to support it!"
                 )
             except Exception as e:
@@ -366,13 +394,15 @@ class UnslothVisionDataCollator:
         except Exception as e:
             raise RuntimeError(e)
         return
+
     pass
 
     def __call__(self, examples):
         # [TODO] Support non image inputs as well
         # The issue is batch = self.processor( forces tensors to be returned and not None.
-        texts  = []
+        texts = []
         images = []
+        videos = []
 
         if self.formatting_func is not None:
             examples = [self.formatting_func(example) for example in examples]
@@ -388,22 +418,22 @@ class UnslothVisionDataCollator:
             # Check if data format is correct for VLMs!
             if len(messages) != 0:
                 message = messages[0]
-                assert(type(message) is dict)
+                assert (type(message) is dict)
                 if "role" not in message and "content" not in message:
                     raise TypeError(
-                        "Unsloth: Failed to use vision data collator!\n"\
+                        "Unsloth: Failed to use vision data collator!\n" \
                         "Maybe use `standardize_data_formats` first!"
                     )
                 content = message["content"]
                 if type(content) is str:
-                    message["content"] = content = [{"type" : "text", "text" : content}]
+                    message["content"] = content = [{"type": "text", "text": content}]
                 elif type(content) is list or type(content) is tuple:
                     part = content[0]
-                    assert("type" in part)
+                    assert ("type" in part)
                 else:
                     raise TypeError(
-                        "Unsloth: Failed to use vision data collator!\n"\
-                        "Your messages must be a like:\n"\
+                        "Unsloth: Failed to use vision data collator!\n" \
+                        "Your messages must be a like:\n" \
                         "[{'role':'user', 'content':[{'type':'text', 'text':'Hello!'}]}]"
                     )
                 pass
@@ -419,8 +449,8 @@ class UnslothVisionDataCollator:
             pass
             message = self.processor.apply_chat_template(
                 messages,
-                tokenize = False,
-                add_generation_prompt = False,
+                tokenize=False,
+                add_generation_prompt=False,
             )
             texts.append(message)
             # Dataset with 2 columns messages / images
@@ -444,42 +474,66 @@ class UnslothVisionDataCollator:
                             image[i] = img.resize((image_size, hsize), LANCZOS)
             pass
             images.append(image)
+            videos.append(video)
         pass
 
         # Tokenize the texts and process the images
-        batch = self.processor(
-            text    = texts,
-            images  = images,
-            padding = True,
-            truncation = self.truncation,
-            max_length = self.max_seq_length,
-            return_tensors = "pt",
-            add_special_tokens = False, # Stop double BOS
-        )
+        batch = self.processor(text=texts, images=None, videos=videos, return_tensors="pt",
+                               padding=True)  # Encode texts and images into tensors
+        # batch = self.processor(
+        #     text    = texts,
+        #     images  = None,
+        #     videos=videos,
+        #     padding = True,
+        #     truncation = self.truncation,
+        #     max_length = self.max_seq_length,
+        #     return_tensors = "pt",
+        #     add_special_tokens = False, # Stop double BOS
+        # )
         # Cannot remove due to bidirectional attention from Gemma 3!
         # batch.pop("token_type_ids", None)
 
-        # Pixtral accepts multiple images, so we have to cast it individually
-        pixel_values = batch["pixel_values"]
-        if type(pixel_values) is list:
-            for j, pixel_value_j in enumerate(pixel_values):
-                if type(pixel_value_j) is list:
-                    for k, pixel_value_k in enumerate(pixel_value_j):
-                        pixel_value_j[k] = pixel_value_k.to(self.dtype)
-                else:
-                    pixel_values[j] = pixel_value_j.to(self.dtype)
-            pass
-            batch["pixel_values"] = pixel_values
-        else:
-            batch["pixel_values"] = batch["pixel_values"].to(self.dtype)
-        pass
+        # # Pixtral accepts multiple images, so we have to cast it individually
+        # pixel_values = batch["pixel_values"]
+        # if type(pixel_values) is list:
+        #     for j, pixel_value_j in enumerate(pixel_values):
+        #         if type(pixel_value_j) is list:
+        #             for k, pixel_value_k in enumerate(pixel_value_j):
+        #                 pixel_value_j[k] = pixel_value_k.to(self.dtype)
+        #         else:
+        #             pixel_values[j] = pixel_value_j.to(self.dtype)
+        #     pass
+        #     batch["pixel_values"] = pixel_values
+        # else:
+        #     batch["pixel_values"] = batch["pixel_values"].to(self.dtype)
+        # pass
 
-        # Mask image tokens and pad tokens
-        labels = batch["input_ids"].clone()
-        labels[torch.isin(labels, self.padding_token_ids)] = self.ignore_index
-        batch["labels"] = labels
-        if self.train_on_responses_only:
-            batch["labels"] = self.train_on_responses_only(batch)["labels"]
+        # The labels are the input_ids, and we mask the padding tokens in the loss computation
+        labels = batch["input_ids"].clone()  # Clone input IDs for labels
+        labels[labels == self.processor.tokenizer.pad_token_id] = -100  # Mask padding tokens in labels
+
+        # Ignore the image token index in the loss computation (model specific)
+        if isinstance(self.processor, Qwen2VLProcessor):  # Check if the processor is Qwen2VLProcessor
+            image_tokens = [151652, 151653, 151655]  # Specific image token IDs for Qwen2VLProcessor
+        else:
+            image_tokens = [
+                self.processor.tokenizer.convert_tokens_to_ids(self.processor.image_token)]  # Convert image token to ID
+
+        # Mask image token IDs in the labels
+        for image_token_id in image_tokens:
+            labels[labels == image_token_id] = -100  # Mask image token IDs in labels
+
+        batch["labels"] = labels  # Add labels to the batch
+
+        # # Mask image tokens and pad tokens
+        # labels = batch["input_ids"].clone()
+        # labels[torch.isin(labels, self.padding_token_ids)] = self.ignore_index
+        # batch["labels"] = labels
+        # if self.train_on_responses_only:
+        #     batch["labels"] = self.train_on_responses_only(batch)["labels"]
         return batch
+
     pass
+
+
 pass
